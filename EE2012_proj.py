251e93329fa5f385c09b30a52ece819403db04b8
Lingke <linkegvh@gmail.com>
2019-11-11 19:18:58 +0800

    adding comments to Q1

--- EE2012_proj.py
+++ EE2012_proj.py
@@ -1,20 +1,40 @@
 """
-created by Ding Lingke A0189238N
+created by Ding Lingke 
+Matriculation Number: A0189238N
 
 11/11/2019
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""
+Input: number of years that you expect the electrical component to function
+
+Output: the pdf value of the following equation:
+    ft = lamda * e^(-lamda * t) in which lamda = 1 / expected
+"""
 def ft(expected, t):
     lam = 1 / expected
     return lam * np.exp(- lam * t)
+
+"""
+Input: number of years that you expect the electrical component to function
+
+Output: the cdf value of the following equation:
+    ft = lamda * e^(-lamda * t) in which lamda = 1 / expected
+"""
 
 def Ft(expected, t):
     lam = 1 / expected
     return 1 - np.exp( - lam * t)
 
+"""
+Input: None
+
+Output: a graph that contains the probability density function
+    and cumulative density function of a continuous function with this pdf: ft = lamda * e^(-lamda * t) 
+    in which lamda = 1 / expected years for the electrical component to function
+"""
 def pdf_cdf():
     t = np.linspace(0, 20, 10000)
     
@@ -36,6 +56,12 @@ def pdf_cdf():
     plt.xlabel('$t$')
     plt.ylabel('$Probability$')
     plt.show()
+"""
+Input: Number years that the electrical component will last beyond
+
+Output: the probability of the electrical component that will last beyond the input amount of years for 
+    expected number of functional years as 3, 4 and 5
+"""
 
 def probability_beyond_x_years(x):
     print("This is the probability of a component that lasts beyong", x, "years")
@@ -44,6 +70,12 @@ def probability_beyond_x_years(x):
     print("If the expected lifetime is 4, the probability is", 1 - Ft(4, x))
     print("If the expected lifetime is 5, the probability is", 1 - Ft(5, x))
     
+
+"""
+Input: None
+
+Output: Run the above program for at least once
+"""
 if __name__ == '__main__':
     pdf_cdf()
     x = 5
